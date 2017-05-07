ff2786c50fc3d1e5fc0b81ae47ed40a7a3c6d039
Fatih BAKIR <mfatihbakir@gmail.com>
2017-05-07 19:20:16 +0300

    moved some stuff to core

--- include/malt_basic/input.hpp
+++ include/malt_basic/input.hpp
@@ -2,19 +2,18 @@
 // Created by Mehmet Fatih BAKIR on 30/04/2017.
 //
 
-#ifndef MALT_INPUT_HPP
-#define MALT_INPUT_HPP
+#pragma once
+
+#include <malt/engine_impl.hpp>
 
 namespace malt
 {
     namespace input
     {
-        bool get_key_up(int key_code);
-        bool get_key(int key_code);
-        bool get_key_down(int key_code);
+        bool get_key_up(int key_code) MALT_WEAK_SYMBOL;
+        bool get_key(int key_code) MALT_WEAK_SYMBOL;
+        bool get_key_down(int key_code) MALT_WEAK_SYMBOL;
 
-        glm::vec2 get_cursor();
+        glm::vec2 get_cursor() MALT_WEAK_SYMBOL;
     }
 }
-
-#endif //MALT_INPUT_HPP
