3f20d43596b2fb00a836778b8bbd9619f0da15a6
Fatih BAKIR <mfatihbakir@gmail.com>
2017-05-20 20:20:49 +0300

    rotate teapot demo

---
+++ components/rotate_comp.cpp
@@ -0,0 +1,22 @@
+//
+// Created by fatih on 5/20/17.
+//
+
+#include <malt_basic/components/rotate_comp.hpp>
+#include <malt/component_mgr_impl.hpp>
+#include <malt_basic/components/transform.hpp>
+
+void rotate_comp::Handle(malt::update)
+{
+    auto t = get_component<malt::transform>();
+    t->rotate(glm::vec3{0, 15, 0} * malt::time::get_delta_time(), malt::space::world);
+}
+
+void rotate_comp::Handle(malt::start)
+{
+    auto t = get_component<malt::transform>();
+    t->rotate(glm::vec3{0, 0, 45}, malt::space::self);
+    t->translate({0, -2.5f, 5});
+}
+
+MALT_IMPLEMENT_COMP(rotate_comp)
---
+++ include/malt_basic/components/rotate_comp.hpp
@@ -0,0 +1,16 @@
+//
+// Created by fatih on 5/20/17.
+//
+
+#pragma once
+
+#include <malt/component.hpp>
+
+class rotate_comp : public malt::component
+{
+public:
+    void Handle(malt::start);
+    void Handle(malt::update);
+};
+
+
